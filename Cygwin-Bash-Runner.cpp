b513cd5d15e7fda5f29376daf258211b006456e1
AdamRGrey <adam.r.grey@gmail.com>
2014-04-10 19:24:09 -0400

    basic project setup
    no actual work started yet, yak-shaving is almost complete

---
+++ Cygwin-Bash-Runner.cpp
@@ -0,0 +1,13 @@
+// Cygwin-Bash-Runner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("yeah yeah I know never use system calls yadda yadda\n");
+	system("pause");
+	return 0;
+}
+
