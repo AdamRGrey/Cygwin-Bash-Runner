ad7ad364c5d129c03057dcd2dfc487ea81d274d9
AdamRGrey <adam.r.grey@gmail.com>
2014-04-13 00:23:25 -0400

    works but flashes console window

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,44 @@ namespace Cygwin_Bash_Runner
 {
     class Program
     {
+        public const string CygpathLocation = "D:\\cygwin\\bin\\cygpath.exe";
+        public const string ShellLocation = "D:\\cygwin\\bin\\bash.exe";
         static void Main(string[] args)
         {
-            for (int i = 0; i < args.Length; i++)
+            var cygpathd = getOutput(CygpathLocation, args[0]);
+            
+            var commandargs =" --login -c \"cd \"" + getOutput(CygpathLocation, Directory.GetCurrentDirectory()) + "\"; '" + cygpathd + "' ";
+            for (int i = 1; i < args.Length; i++)
             {
-                Console.WriteLine(args[i]);   
+                commandargs += args[i];
             }
-            Console.ReadLine();
+
+            new Process { StartInfo = { FileName = ShellLocation, Arguments = commandargs } }.Start();
+        }
+        public static string getOutput(string program, string arg)
+        {
+            var toReturn = "";
+            var proc = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false, 
+                    RedirectStandardOutput = true, 
+                    RedirectStandardError = true,
+                    FileName = program,
+                    Arguments = arg
+                },
+                EnableRaisingEvents = true
+            };
+            proc.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => { toReturn += e.Data + " "; };
+            proc.OutputDataReceived += (object sender, DataReceivedEventArgs e) => { toReturn += e.Data + " "; };
+
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
+            proc.WaitForExit();
+
+            return toReturn.TrimEnd(' ');
         }
     }
 }
